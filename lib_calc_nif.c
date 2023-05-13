fcbcbd525806784b4bf26c9552e9e19824fcbf21
Philip Munksgaard <philip@munksgaard.me>
2023-05-13 21:54:27 +0200

    Remove calc stuff

--- lib_calc_nif.c
+++ lib_calc_nif.c
@@ -1,5 +1,4 @@
 #include <erl_nif.h>
-#include "lib_calc.h"
 #include "lib_map.c"
 
 struct futhark_context;
@@ -40,42 +39,6 @@ static int load(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info)
     atom_ok = enif_make_atom(env, "ok");
 
     return 0;
-}
-
-static ERL_NIF_TERM somar_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
-  int a, b, result;
-  enif_get_int(env, argv[0], &a);
-  enif_get_int(env, argv[1], &b);
-  result = somar(a, b);
-  return enif_make_int(env, result);
-}
-
-static ERL_NIF_TERM subtrair_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
-  int a, b, result;
-  enif_get_int(env, argv[0], &a);
-  enif_get_int(env, argv[1], &b);
-  result = subtrair(a, b);
-  return enif_make_int(env, result);
-}
-
-static ERL_NIF_TERM multiplicar_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
-  int a, b, result;
-  enif_get_int(env, argv[0], &a);
-  enif_get_int(env, argv[1], &b);
-  result = multiplicar(a, b);
-  return enif_make_int(env, result);
-}
-
-static ERL_NIF_TERM dividir_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
-  int a, b, result;
-  enif_get_int(env, argv[0], &a);
-  enif_get_int(env, argv[1], &b);
-  result = dividir(a, b);
-  return enif_make_int(env, result);
 }
 
 static ERL_NIF_TERM futhark_context_config_new_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
@@ -130,10 +93,6 @@ static ERL_NIF_TERM futhark_context_new_nif(ErlNifEnv* env, int argc, const ERL_
 }
 
 static ErlNifFunc nif_funcs[] = {
-  {"somar", 2, somar_nif},
-  {"subtrair", 2, subtrair_nif},
-  {"multiplicar", 2, multiplicar_nif},
-  {"dividir", 2, dividir_nif},
   {"futhark_context_config_new", 0, futhark_context_config_new_nif},
   {"futhark_context_new", 1, futhark_context_new_nif}
 };
