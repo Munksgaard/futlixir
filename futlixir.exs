9f41586c383023d98fbc9adf0b4d251b6b8565b8
Philip Munksgaard <philip@munksgaard.me>
2023-05-14 22:06:40 +0200

    More work on futlixir

--- futlixir.exs
+++ futlixir.exs
@@ -60,6 +60,107 @@ defmodule Futlexir.EX do
   end
 end
 
+defmodule Futlexir.NIF do
+  def boilerplate(rootname) do
+    ~s"""
+    #include <erl_nif.h>
+    #include "#{rootname}.c"
+
+    struct futhark_context;
+
+    ERL_NIF_TERM atom_ok;
+
+    ErlNifResourceType* CONFIG_TYPE;
+    ErlNifResourceType* CONTEXT_TYPE;
+    """
+  end
+
+  def open_resources(types) do
+    ~s"""
+
+    static int open_resource(ErlNifEnv* env, const char* name)
+    {
+      const char* mod = "resources";
+      int flags = ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER;
+
+      CONTEXT_TYPE = enif_open_resource_type(env, mod, name, NULL, flags, NULL);
+      if(CONFIG_TYPE == NULL) return -1;
+      return 0;
+    }
+
+    static int load(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info)
+    {
+      if(open_resource(env, "Config") == -1) return -1;
+      if(open_resource(env, "Context") == -1) return -1;
+    #{open_types(types)}
+
+      atom_ok = enif_make_atom(env, "ok");
+
+      return 0;
+    }
+
+    static ERL_NIF_TERM futhark_context_config_new_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+    {
+      struct futhark_context_config **res;
+
+      ERL_NIF_TERM ret;
+
+      if(argc != 0) {
+        return enif_make_badarg(env);
+      }
+
+      res = enif_alloc_resource(CONFIG_TYPE, sizeof(struct futhark_context_config *));
+      if(res == NULL) return enif_make_badarg(env);
+
+      struct futhark_context_config* tmp = futhark_context_config_new();
+
+      *res = tmp;
+
+      ret = enif_make_resource(env, res);
+      enif_release_resource(res);
+
+      return enif_make_tuple2(env, atom_ok, ret);
+    }
+
+    static ERL_NIF_TERM futhark_context_new_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+    {
+      struct futhark_context_config **cfg;
+      struct futhark_context **res;
+
+      ERL_NIF_TERM ret;
+
+      if(argc != 1) {
+        return enif_make_badarg(env);
+      }
+
+      if(!enif_get_resource(env, argv[0], CONFIG_TYPE, (void**) &cfg)) {
+        return enif_make_badarg(env);
+      }
+
+      res = enif_alloc_resource(CONTEXT_TYPE, sizeof(struct futhark_context *));
+      if(res == NULL) return enif_make_badarg(env);
+
+      struct futhark_context* tmp = futhark_context_new(*cfg);
+
+      *res = tmp;
+
+      ret = enif_make_resource(env, res);
+      enif_release_resource(res);
+
+      return enif_make_tuple2(env, atom_ok, ret);
+    }
+
+    """
+  end
+
+  def open_types(types) do
+    for {_, %{"elemtype" => elemtype, "rank" => rank}} <- types do
+      "  if(open_resource(env, \"#{elemtype}_#{rank}d\") == -1) return -1;"
+    end
+    |> Enum.join("\n")
+  end
+end
+
 defmodule Futlexir.CLI do
   def main(args \\ []) do
     :ok =
@@ -108,15 +209,18 @@ defmodule Futlexir.CLI do
     end
   end
 
+  def print_nif_resources(device, types) do
+    for {_ty, %{"elemtype" => elemtype, "rank" => rank}} <- types do
+      resource_name = String.upcase("#{elemtype}_#{rank}d")
+      IO.puts(device, "ErlNifResourceType* #{resource_name};")
+    end
+  end
+
   defp write_nif_file(rootname, module_name, manifest) do
     with {:ok, nif_file} <- File.open(rootname <> "_nif.c", [:write]) do
-      IO.puts(nif_file, ~s"""
-      #include <erl_nif.h>
-      #include "lib_map.c"
-
-      struct futhark_context;
-      """)
-
+      IO.puts(nif_file, Futlexir.NIF.boilerplate(rootname))
+      print_nif_resources(nif_file, manifest["types"])
+      IO.puts(nif_file, Futlexir.NIF.open_resources(manifest["types"]))
       File.close(nif_file)
       :ok
     end
